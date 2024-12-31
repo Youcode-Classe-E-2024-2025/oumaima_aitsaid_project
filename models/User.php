cf5bfb38b7719f24d7b33baa66569fa66944236b
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2024-12-31 17:17:59 +0100

    adding construct function & setters in user

--- models/User.php
+++ models/User.php
@@ -8,14 +8,25 @@ class User {
     private $conn;
     private $table_name = "users";
 
-   
+    public function __construct($db){
+        $this->conn = $db;
+    }
 
-    
+    public function setName($name){
+        $this->name = htmlspecialchars(strip_tags($name));
+    }
 
-    
+    public function setEmail($email){
+        $this->email = htmlspecialchars(strip_tags($email));
+    }
 
-   
-   
+    public function setPassword($password){
+        $this->password = $password;
+    }
+
+    public function setRole($role){
+        $this->role = $role ?? 'team_member';
+    }
 
     
 
