64037aa36d06b78d73174f62c5555bc984a06dc5
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2024-12-31 17:19:59 +0100

    adding function register in class AuthController

--- controllers/Authcontroller.php
+++ controllers/Authcontroller.php
@@ -10,6 +10,27 @@ public function __construct(){
     $this->user=new User($this->db);
 }
 
+public function register(){
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        $this->user->setName($_POST['username'] ?? '');
+        $this->user->setEmail($_POST['email'] ?? '');
+        $this->user->setPassword($_POST['password'] ?? '');
+        $this->user->setRole('team_member');
+        
+        $errors = $this->user->validate();
+        if(empty($errors)){
+            if($this->user->create()){
+                header("Location: index.php?action=login&register=1");
+                exit();
+            } else {
+                $errors[] = "Problem creating user";
+            }
+        }
+        include 'views/register.php';
+    } else {
+        include 'views/register.php';
+    }
+}
 
 
 }
