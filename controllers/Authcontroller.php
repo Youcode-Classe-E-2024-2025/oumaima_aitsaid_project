6694c5da63f489b89026b46c3d27041a498bd10e
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2024-12-31 17:20:47 +0100

    adding function login in class Authocontroller

--- controllers/Authcontroller.php
+++ controllers/Authcontroller.php
@@ -32,6 +32,44 @@ public function register(){
     }
 }
 
+public function login(){
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+$email=$_POST['email'] ?? '';
+$password =$_POST['password'] ?? '';
+
+if(empty($email) || empty($password))
+$error ="remplire les champs svp";
+
+include 'views/login.php';
+return;
+
+
+$stmt =$this->db->prepare("SELECT * FROM users WHERE email= :email");
+$stmt ->bindParam(':email',$email);
+$stmt->execute();
+$user=$stmt->fetch(PDO::FETCH_ASSOC);
+
+if($user && password_verify($password,$user['password'])){
+      $_SESSION['user_id'] =$user['id'];
+      $_SESSION['user_name'] =$user['name'];
+      header("location:index.php?action:dashboard");
+      exit();
+
+
+}
+else{
+    $error ="Invalid email or password";
+    include 'views/login.php';
+}
+
+}
+else {
+    include 'views/login.php';
+}
+
+
+}
 
 }
 
