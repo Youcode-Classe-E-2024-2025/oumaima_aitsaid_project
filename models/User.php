a64deca88cc8754dd1a8031f9455c456b7fdb6f5
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2024-12-31 17:17:12 +0100

    adding class User in modeles

--- models/User.php
+++ models/User.php
@@ -1,1 +1,24 @@
 <?php 
+class User {
+    private $id;
+    private $name;
+    private $email;
+    private $password;
+    private $role;
+    private $conn;
+    private $table_name = "users";
+
+   
+
+    
+
+    
+
+   
+   
+
+    
+
+    
+}
+?>
