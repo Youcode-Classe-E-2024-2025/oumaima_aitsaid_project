7addb485d0473e13d5730f2980c59de102815988
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2025-01-01 17:16:23 +0100

    Implement createProject method in Project model

--- models/Project.php
+++ models/Project.php
@@ -24,6 +24,21 @@ class Project {
         return false;
     }
 
+    public function createProject() {
+        $query = "INSERT INTO " . $this->table_name . " (name, description, date_commence, date_fin, status) VALUES (:name, :description, :date_commence, :date_fin, :status)";
+        $stmt = $this->conn->prepare($query);
+
+        $stmt->bindParam(":name", $this->name);
+        $stmt->bindParam(":description", $this->description);
+        $stmt->bindParam(":date_commence", $this->date_commence);
+        $stmt->bindParam(":date_fin", $this->date_fin);
+        $stmt->bindParam(":status", $this->status);
+
+        if ($stmt->execute()) {
+            return true;
+        }
+        return false;
+    }
  
 
 
