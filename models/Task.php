e771ea3147d03238df2d84b974829748f6be5524
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2025-01-02 13:13:25 +0100

    adding function create Task

--- models/Task.php
+++ models/Task.php
@@ -3,6 +3,7 @@
 class Task {
     private $conn;
     private $table_name = "tasks";
+
     public $id;
     public $title;
     public $description;
@@ -19,7 +20,43 @@ class Task {
         $this->conn = $db;
     }
 
-    
+    public function createTask() {
+        $query = "INSERT INTO " . $this->table_name . " 
+                  (title, description, status, priority, fin_date, category_id, project_id, assigned_to, created_by) 
+                  VALUES 
+                  (:title, :description, :status, :priority, :fin_date, :category_id, :project_id, :assigned_to, :created_by)";
+
+        $stmt = $this->conn->prepare($query);
+
+        $this->title = htmlspecialchars(strip_tags($this->title));
+        $this->description = htmlspecialchars(strip_tags($this->description));
+        $this->status = $this->validateStatus($this->status);
+        $this->priority = $this->validatePriority($this->priority);
+        $this->fin_date = htmlspecialchars(strip_tags($this->fin_date));
+        $this->category_id = $this->category_id;
+        $this->project_id = $this->project_id;
+        $this->assigned_to = $this->assigned_to;
+        $this->created_by = htmlspecialchars(strip_tags($this->created_by));
+
+        $stmt->bindParam(":title", $this->title);
+        $stmt->bindParam(":description", $this->description);
+        $stmt->bindParam(":status", $this->status);
+        $stmt->bindParam(":priority", $this->priority);
+        if (empty($this->fin_date)) {
+            $stmt->bindValue(":fin_date", null, PDO::PARAM_NULL);
+        } else {
+            $stmt->bindParam(":fin_date", $this->fin_date);
+        }
+            $stmt->bindParam(":category_id", $this->category_id);
+        $stmt->bindParam(":project_id", $this->project_id);
+        $stmt->bindParam(":assigned_to", $this->assigned_to);
+        $stmt->bindParam(":created_by", $this->created_by);
+
+        if ($stmt->execute()) {
+            return $this->conn->lastInsertId();
+        }
+        return false;
+    }
 
    
 
