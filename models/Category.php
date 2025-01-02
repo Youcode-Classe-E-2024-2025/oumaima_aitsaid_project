87b626923645098c55935b77c66b4c3444a1fec5
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2025-01-02 14:55:09 +0100

    - Secured input data with htmlspecialchars and strip_tags to prevent XSS attacks. - Added methods:   - createCategory()   - getAllCategories()   - getCategoryById()   - updateCategory()   - deleteCategory()

--- models/Category.php
+++ models/Category.php
@@ -12,5 +12,72 @@ class Category {
         $this->conn = $db;
     }
 
+    public function createCategory() {
+        $query = "INSERT INTO " . $this->table_name . " (name, description) VALUES (:name, :description)";
+
+        $stmt = $this->conn->prepare($query);
+
+        $this->name = htmlspecialchars(strip_tags($this->name));
+        $this->description = htmlspecialchars(strip_tags($this->description));
+
+        $stmt->bindParam(":name", $this->name);
+        $stmt->bindParam(":description", $this->description);
+
+        if ($stmt->execute()) {
+            return $this->conn->lastInsertId();
+        }
+        return false;
+    }
+
+    public function getAllCategories() {
+        $query = "SELECT * FROM " . $this->table_name;
+        $stmt = $this->conn->prepare($query);
+        
+        if ($stmt->execute()) {
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+        return false;
+    }
+
+    public function getCategoryById($id) {
+        $query = "SELECT * FROM " . $this->table_name . " WHERE id = :id";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(":id", $id);
+        
+        if ($stmt->execute()) {
+            return $stmt->fetch(PDO::FETCH_ASSOC);
+        }
+        return false;
+    }
+
+    public function updateCategory() {
+        $query = "UPDATE " . $this->table_name . " SET name = :name, description = :description WHERE id = :id";
+
+        $stmt = $this->conn->prepare($query);
+
+        $this->name = htmlspecialchars(strip_tags($this->name));
+        $this->description = htmlspecialchars(strip_tags($this->description));
+
+        $stmt->bindParam(":name", $this->name);
+        $stmt->bindParam(":description", $this->description);
+        $stmt->bindParam(":id", $this->id);
+
+        if ($stmt->execute()) {
+            return true;
+        }
+        return false;
+    }
+
+    public function deleteCategory() {
+        $query = "DELETE FROM " . $this->table_name . " WHERE id = :id";
+        $stmt = $this->conn->prepare($query);
+
+        $stmt->bindParam(":id", $this->id);
+
+        if ($stmt->execute()) {
+            return true;
+        }
+        return false;
+    }
 }
 
