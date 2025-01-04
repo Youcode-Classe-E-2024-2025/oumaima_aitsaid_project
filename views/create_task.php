a1aae43a100d6a32a91bc7aaa0f9be8462bf9643
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2025-01-04 21:50:52 +0100

    adding tags selection in create tasks

--- views/create_task.php
+++ views/create_task.php
@@ -95,7 +95,21 @@
             </div>
 
             <!-- Tags Selection -->
-           
+            <div class="mb-4">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="tags">
+                    Tags
+                </label>
+                <select multiple class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="tags" name="tags[]">
+                    <?php if (!empty($tags)): ?>
+                        <?php foreach ($tags as $tag): ?>
+                            <option value="<?php echo htmlspecialchars($tag['id']); ?>"><?php echo htmlspecialchars($tag['name']); ?></option>
+                        <?php endforeach; ?>
+                    <?php else: ?>
+                        <option value="" disabled>No tags </option>
+                    <?php endif; ?>
+                </select>
+            </div>
+
             <!-- Submit and Cancel Buttons -->
             <div class="flex items-center justify-between">
                 <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
