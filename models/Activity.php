c4fcd114fcb81777b82aa58d2668b06ccaa1a731
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2025-01-07 14:24:45 +0100

    add function logActivity pour inserer les nouvelle activité

--- models/Activity.php
+++ models/Activity.php
@@ -14,7 +14,29 @@ class Activity {
         $this->conn = $db;
     }
 
-   
+    public function logActivity() {
+        $query = "INSERT INTO " . $this->table_name . " 
+                  (project_id, user_id, action, description) 
+                  VALUES 
+                  (:project_id, :user_id, :action, :description)";
+
+        $stmt = $this->conn->prepare($query);
+
+        $this->project_id = htmlspecialchars(strip_tags($this->project_id));
+        $this->user_id = htmlspecialchars(strip_tags($this->user_id));
+        $this->action = htmlspecialchars(strip_tags($this->action));
+        $this->description = htmlspecialchars(strip_tags($this->description));
+
+        $stmt->bindParam(":project_id", $this->project_id);
+        $stmt->bindParam(":user_id", $this->user_id);
+        $stmt->bindParam(":action", $this->action);
+        $stmt->bindParam(":description", $this->description);
+
+        if ($stmt->execute()) {
+            return true;
+        }
+        return false;
+    }
 
   
 }
